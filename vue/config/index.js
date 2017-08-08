b96f2f001c2d3ba68f050fe64c7652de70d000b4
eeo_lqy <qingyi.luan@eeoa.com>
2017-08-08 11:24:56 +0800

    no message

--- vue/config/index.js
+++ vue/config/index.js
@@ -4,8 +4,8 @@ var path = require('path')
 module.exports = {
   build: {
     env: require('./prod.env'),
-    index: path.resolve(__dirname, '../../project/dist/index.html'),
-    assetsRoot: path.resolve(__dirname, '../../project/dist'),
+    index: path.resolve(__dirname, '../../dist/index.html'),
+    assetsRoot: path.resolve(__dirname, '../../dist'),
     assetsSubDirectory: 'static',
     assetsPublicPath: '/project/dist',
     productionSourceMap: true,
